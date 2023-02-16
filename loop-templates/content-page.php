e32a727929686c5d9d6e082cb78be70954ac94b7
patrikhorat <patrik.horat@eightytwo.ch>
2023-02-16 17:08:02 +0100

    vorlage content page default

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -11,17 +11,6 @@ defined( 'ABSPATH' ) || exit;
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<?php
-	if ( ! is_page_template( 'page-templates/no-title.php' ) ) {
-		the_title(
-			'<header class="entry-header"><h1 class="entry-title">',
-			'</h1></header><!-- .entry-header -->'
-		);
-	}
-
-	echo get_the_post_thumbnail( $post->ID, 'large' );
-	?>
-
 	<div class="entry-content">
 
 		<?php
