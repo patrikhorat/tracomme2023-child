2f3444825478a1ba779da33d50236475e7cb3b09
patrikhorat <patrik.horat@eightytwo.ch>
2023-05-30 16:15:43 +0200

    search form texte

--- searchform.php
+++ searchform.php
@@ -18,14 +18,14 @@ if ( isset( $args['aria_label'] ) && ! empty( $args['aria_label'] ) ) {
 ?>
 
 <form role="search" class="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" <?php echo $aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?>>
-	<label class="screen-reader-text" for="<?php echo $uid; ?>"><?php echo esc_html_x( 'Search for:', 'label', 'tracomme2023' ); ?></label>
+	<label class="screen-reader-text" for="<?php echo $uid; ?>"><?php echo esc_html_x( 'Search for:', 'label', 'tracomme2023-child' ); ?></label>
 	<div class="input-group">
-		<input type="search" class="field search-field form-control" id="<?php echo $uid; ?>" name="s" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'tracomme2023' ); ?>">
+		<input type="search" class="field search-field form-control" id="<?php echo $uid; ?>" name="s" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'tracomme2023-child' ); ?>">
 		<?php if ( 'bootstrap5' === $bootstrap_version ) : ?>
-			<input type="submit" class="submit search-submit btn btn-primary" name="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'tracomme2023' ); ?>">
+			<input type="submit" class="submit search-submit btn btn-primary" name="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'tracomme2023-child' ); ?>">
 		<?php else : ?>
 			<span class="input-group-append">
-				<input type="submit" class="submit search-submit btn btn-primary" name="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'tracomme2023' ); ?>">
+				<input type="submit" class="submit search-submit btn btn-primary" name="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'tracomme2023-child' ); ?>">
 			</span>
 		<?php endif; ?>
 	</div>
