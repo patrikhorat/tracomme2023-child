16d3d1bc54ca9920bfb5c7940406af8a7f179cd3
patrikhorat <patrik.horat@eightytwo.ch>
2024-03-02 12:09:26 +0100

    Google Tag Manager

--- header.php
+++ header.php
@@ -24,9 +24,20 @@ $navbar_type       = get_theme_mod( 'tracomme2023_navbar_type', 'collapse' );
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 	<script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=6471ac9d413e9c001905a2d3&product=inline-share-buttons&source=platform" async="async"></script>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-NT5RKW63');</script>
+	<!-- End Google Tag Manager -->
 </head>
 
 <body <?php body_class(); ?> <?php tracomme2023_body_attributes(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NT5RKW63"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php do_action( 'wp_body_open' ); ?>
 <div class="site" id="page">
 
